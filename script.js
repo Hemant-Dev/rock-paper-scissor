503f4c67b8ee37de05a1f36c61cff7b55027e813
Hemant <53123263+Hemant-Dev@users.noreply.github.com>
2023-12-07 16:00:55 +0000

    Created Various new Functions Implemented a single round logic func and getting random computer input func

--- script.js
+++ script.js
@@ -1,1 +1,63 @@
-console.log("Hello World!");
+// console.log("Hello World!");
+function getComputerSelection(){
+    let movesArr = ["rock", "paper", "scissor"];
+    let randomMoveIndex = getRandomInt(0,2);
+    return movesArr[randomMoveIndex];
+}
+function getRandomInt(min, max){
+    return Math.floor(Math.random() * (max-min+1)) + min;
+}
+let userMove = "rock";
+
+let computerMove = getComputerSelection();
+let winner = "";
+let points = 0;
+function playRound(userMove, computerMove){
+    if(userMove === "rock"){
+        if(computerMove === "scissor"){
+            winner = "User";
+            points = 1;
+        }
+        else if(computerMove === "paper"){
+            winner = "Computer";
+            points = 1;
+        }
+        else{
+            winner = "Both";
+            points = 0;
+        }
+        
+    }
+    else if(userMove === "paper"){
+        if(computerMove === "rock"){
+            winner = "User";
+            points = 1;
+        }
+        else if(computerMove === "scissor"){
+            winner = "Computer";
+            points = 1;
+        }
+        else{
+            winner = "Both";
+            points = 0;
+        }
+        
+    }
+    else if(userMove === "scissor"){
+        if(computerMove === "rock"){
+            winner = "Computer";
+            points = 1;
+        }
+        else if(computerMove === "paper"){
+            winner = "User";
+            points = 1;
+        }
+        else{
+            winner = "Both";
+            points = 0;
+        }
+    }
+    console.log("User Move: " + userMove + " Computer Move: " + computerMove);
+    console.log("Winner : " + winner + " Points: " + points);
+}
+playRound(userMove, computerMove);
